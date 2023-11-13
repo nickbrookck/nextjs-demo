df5b761bb07b4325d7f28b3cba9313850612e75a
nick <nickbrook72@gmail.com>
2023-11-13 11:30:44 +0000

    Trivial NextJS app and Cypress test

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -4,6 +4,7 @@ describe('template spec', () => {
     cy.get('h1').should('contain.text', 'Really simple');
     cy.findByTestId('next-page-button').click();
     cy.get('h2').should('contain.text', 'next page');
+    cy.findByTestId('search-box').type('Hello');
     cy.findByTestId('back-button');
   })
 })
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,27 @@
 import Link from 'next/link';
 import {Button} from 'primereact/button';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
+import { InputText } from 'primereact/inputtext';
+import { Panel } from 'primereact/panel';
+
 
 export default function Dashboard() {
   return (
     <div>
       <h2>This is the next page</h2>
 
-      <Link href=".." data-testid="back-button">
-        <Button label="Go back">
+      <Panel header="Stuff" className="m-1">
 
-        </Button>
-      </Link>
+
+        <InputText placeholder="Search" data-testid="search-box"/>
+
+        <Link href=".." data-testid="back-button">
+          <Button label="Go back">
+
+          </Button>
+        </Link>
+      </Panel>
+
     </div>
   )
 }
