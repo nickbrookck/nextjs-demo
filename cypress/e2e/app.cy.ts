a805cd4bd6a8ef1409158224c530a76520026970
nick <nickbrook72@gmail.com>
2023-11-13 11:37:01 +0000

    Trivial NextJS app and Cypress test

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('list search page', () => {
+  it('should allow searching', () => {
     cy.visit('/');
     cy.get('h1').should('contain.text', 'Really simple');
     cy.findByTestId('next-page-button').click();
@@ -8,3 +8,5 @@ describe('template spec', () => {
     cy.findByTestId('back-button');
   })
 })
+
+
