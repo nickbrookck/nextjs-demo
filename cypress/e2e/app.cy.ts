a6827b1a7f8fbadec3c114b0a549f3d392b18bf9
Nick Brook <nick@cykubed.com>
2023-11-13 17:23:12 +0000

    Broke the test

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,7 +1,8 @@
 describe('list search page', () => {
   it('should allow searching', () => {
     cy.visit('/');
-    cy.get('h1').should('contain.text', 'Really simple');
+    // Break this!
+    cy.get('h1').should('contain.text', 'Really simpleX');
     cy.findByTestId('next-page-button').click();
     cy.get('h2').should('contain.text', 'next page');
     cy.findByTestId('search-box').type('Hello');
